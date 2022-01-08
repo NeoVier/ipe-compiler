271665e0ba0f86a7eb1c657412a4297867d5111a
Henrique Buss <henrique.buss@hotmail.com>
2022-01-08 09:34:25 -0300

    Adjust elm-review config

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -11,13 +11,25 @@ when inside the directory containing this file.
 
 -}
 
+import CognitiveComplexity
+import NoBooleanCase
 import NoDebug.Log
 import NoDebug.TodoOrToString
 import NoExposingEverything
 import NoImportingEverything
 import NoInconsistentAliases
 import NoMissingTypeAnnotation
+import NoMissingTypeExpose
 import NoModuleOnExposedNames
+import NoPrematureLetComputation
+import NoRedundantConcat
+import NoRedundantCons
+import NoRegex
+import NoSimpleLetBody
+import NoSinglePatternCase
+import NoUnoptimizedRecursion
+import NoUnsortedCases
+import NoUnsortedRecords
 import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
@@ -27,27 +39,41 @@ import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
 import Review.Rule exposing (Rule)
+import Simplify
+import UseCamelCase
 
 
 config : List Rule
 config =
-    [ NoUnused.Modules.rule
-    , NoUnused.Exports.rule
-    , NoUnused.Dependencies.rule
+    [ NoRegex.rule
+    , NoSinglePatternCase.rule NoSinglePatternCase.fixInArgument
+    , NoUnsortedRecords.rule NoUnsortedRecords.defaults
+    , NoUnsortedCases.rule NoUnsortedCases.defaults
+    , NoSimpleLetBody.rule
+    , CognitiveComplexity.rule 15
+    , NoExposingEverything.rule
+    , NoImportingEverything.rule []
+    , NoMissingTypeAnnotation.rule
+    , NoMissingTypeExpose.rule
+    , NoPrematureLetComputation.rule
+    , NoUnoptimizedRecursion.rule (NoUnoptimizedRecursion.optOutWithComment "IGNORE TCO")
+    , Simplify.rule Simplify.defaults
     , NoUnused.CustomTypeConstructorArgs.rule
-
-    --, NoUnused.Variables.rule
-    -- , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
     , NoUnused.Parameters.rule
-
-    --, NoUnused.Patterns.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+    , UseCamelCase.rule UseCamelCase.default
+    , NoModuleOnExposedNames.rule
+    , NoBooleanCase.rule
+    , NoRedundantConcat.rule
+    , NoRedundantCons.rule
     , NoDebug.Log.rule
     , NoDebug.TodoOrToString.rule
         |> Review.Rule.ignoreErrorsForDirectories [ "tests" ]
-    , NoExposingEverything.rule
-
-    --, NoImportingEverything.rule []
-    , NoMissingTypeAnnotation.rule
 
     --, NoInconsistentAliases.config
     --    [ ( "Html.Attributes", "Attr" )
@@ -56,5 +82,4 @@ config =
     --    ]
     --    |> NoInconsistentAliases.noMissingAliases
     --    |> NoInconsistentAliases.rule
-    , NoModuleOnExposedNames.rule
     ]
