219236abcf002968efd3c092d254a589d984dc89
Henrique Buss <henrique.buss@hotmail.com>
2022-01-08 18:19:16 -0300

    Add Parser module and tests

---
+++ src/Ipe/Parser.elm
@@ -0,0 +1,142 @@
+module Ipe.Parser exposing (moduleDefinition)
+
+{-| This is the module responsible for transforming a `String` into an Ipe AST (as in the Ipe.Language module).
+-}
+
+import Ipe.Language as Language
+import Parser exposing ((|.), (|=), Parser)
+import Set exposing (Set)
+
+
+
+-------------- RESERVED KEYWORDS --------------
+
+
+reservedKeywords : Set String
+reservedKeywords =
+    Set.fromList
+        [ "module"
+        , "exposing"
+        ]
+
+
+
+-------------- HELPER FUNCTIONS --------------
+
+
+{-| Parse a name that starts with an uppercase letter. Useful for module names and type names.
+-}
+uppercaseName : Parser String
+uppercaseName =
+    Parser.variable
+        { start = Char.isUpper
+        , inner = \c -> Char.isAlphaNum c || c == '_'
+        , reserved = reservedKeywords
+        }
+
+
+{-| Parse a name that starts with a lowercase letter. Useful for function names and variable names.
+-}
+lowercaseName : Parser String
+lowercaseName =
+    Parser.variable
+        { start = Char.isLower
+        , inner = \c -> Char.isAlphaNum c || c == '_'
+        , reserved = reservedKeywords
+        }
+
+
+
+-------------- MODULE DEFINITION --------------
+
+
+{-| Parse a module definition.
+
+    -- Modules can expose some definitions
+    module ModuleName exposing (OpaqueType, SemiOpaqueType(Variant1, Variant2), VisibleType(..), function)
+
+
+    -- Or all of them
+    module ModuleName exposing (..)
+
+-}
+moduleDefinition : Parser Language.ModuleDefinition
+moduleDefinition =
+    Parser.succeed Language.ModuleDefinition
+        |. Parser.keyword "module"
+        |. Parser.spaces
+        |= uppercaseName
+        |. Parser.spaces
+        |. Parser.keyword "exposing"
+        |. Parser.spaces
+        |= moduleExport
+
+
+{-| -}
+moduleExport : Parser Language.ModuleExport
+moduleExport =
+    Parser.oneOf
+        [ Parser.succeed Language.ExportEverything
+            |. Parser.token "(..)"
+        , Parser.sequence
+            { start = "("
+            , separator = ","
+            , end = ")"
+            , spaces = Parser.spaces
+            , item = exportItem
+            , trailing = Parser.Forbidden
+            }
+            |> Parser.andThen
+                (\exportedItems ->
+                    if List.isEmpty exportedItems then
+                        Parser.problem
+                            """a module needs to export *something*!
+
+You can either export everything in the module using `(..)`, or just some items by providing a comma-separated list, such as `(function1, function2)`.
+
+If you don't want to expose anything from this module, what's the point of having it?"""
+
+                    else
+                        Parser.succeed (Language.ExportSome exportedItems)
+                )
+        ]
+
+
+{-| -}
+exportItem : Parser Language.ExportItem
+exportItem =
+    Parser.oneOf
+        [ lowercaseName
+            |> Parser.map
+                (\functionName ->
+                    Language.ExportFunction { functionName = functionName }
+                )
+        , Parser.succeed
+            (\typeName typeExport_ ->
+                Language.ExportType
+                    { typeName = typeName
+                    , typeExport = typeExport_
+                    }
+            )
+            |= uppercaseName
+            |= typeExport
+        ]
+
+
+{-| -}
+typeExport : Parser Language.TypeExport
+typeExport =
+    Parser.oneOf
+        [ Parser.succeed Language.ExportAllVariants
+            |. Parser.token "(..)"
+        , Parser.sequence
+            { start = "("
+            , separator = ","
+            , end = ")"
+            , spaces = Parser.spaces
+            , item = uppercaseName
+            , trailing = Parser.Forbidden
+            }
+            |> Parser.map Language.ExportSomeVariants
+        , Parser.succeed Language.ExportNoVariant
+        ]
---
+++ tests/Ipe/ParserTest.elm
@@ -0,0 +1,100 @@
+module Ipe.ParserTest exposing (suite)
+
+import Expect
+import Ipe.Language as Language
+import Ipe.Parser
+import Parser
+import Test exposing (Test, test)
+
+
+suite : Test
+suite =
+    Test.describe "The Parser module"
+        [ moduleDefinition
+        ]
+
+
+moduleDefinition : Test
+moduleDefinition =
+    let
+        expectOk input response =
+            Parser.run Ipe.Parser.moduleDefinition input
+                |> Expect.equal (Ok response)
+
+        expectErr input =
+            Parser.run Ipe.Parser.moduleDefinition input
+                |> Expect.err
+    in
+    Test.describe "Ipe.Parser.moduleDefinition"
+        [ test "correctly parses module definition exposing everything" <|
+            \_ ->
+                let
+                    input =
+                        "module A exposing (..)"
+
+                    output =
+                        { moduleName = "A", exports = Language.ExportEverything }
+                in
+                expectOk input output
+        , test "correctly parses module definition with some exposed functions" <|
+            \_ ->
+                let
+                    input =
+                        "module A exposing (function1, function2)"
+
+                    output =
+                        { moduleName = "A"
+                        , exports =
+                            Language.ExportSome
+                                [ Language.ExportFunction { functionName = "function1" }
+                                , Language.ExportFunction { functionName = "function2" }
+                                ]
+                        }
+                in
+                expectOk input output
+        , test "correctly parses module definition with some exposed types" <|
+            \_ ->
+                let
+                    input =
+                        "module A exposing (OpaqueType, SemiOpaqueType(Variant1, Variant2), VisibleType(..))"
+
+                    output =
+                        { moduleName = "A"
+                        , exports =
+                            Language.ExportSome
+                                [ Language.ExportType { typeName = "OpaqueType", typeExport = Language.ExportNoVariant }
+                                , Language.ExportType { typeName = "SemiOpaqueType", typeExport = Language.ExportSomeVariants [ "Variant1", "Variant2" ] }
+                                , Language.ExportType { typeName = "VisibleType", typeExport = Language.ExportAllVariants }
+                                ]
+                        }
+                in
+                expectOk input output
+        , test "does not allow modules that start with a lowercase letter" <|
+            \_ ->
+                let
+                    input =
+                        "module someModule exposing (..)"
+                in
+                expectErr input
+        , test "does not allow to expose variants on functions" <|
+            \_ ->
+                let
+                    input =
+                        "module A exposing (function1(Variant1))"
+                in
+                expectErr input
+        , test "does not allow to expose everything and some things at the same time" <|
+            \_ ->
+                let
+                    input =
+                        "module A exposing (.., function1)"
+                in
+                expectErr input
+        , test "does not allow to expose everything and some things at the same time when exposing function first" <|
+            \_ ->
+                let
+                    input =
+                        "module A exposing (function1, ..)"
+                in
+                expectErr input
+        ]
